4b1adcb618fe38570bf45d72e4475bd0eeedecf6
kilian frederix <kilianfrederix@gmail.com>
2024-04-25 08:25:14 +0200

    a

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -9,18 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-<div id="list-header">
-            <h2>Notes</h2>
-
-            <div class="btn-group btn-group-justified">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-default active">All notes</button>
-                </div>
-
-                <div class="btn-group">
-                    <button type="button" class="btn btn-default">Favorites</button>
-                </div>
-            </div>
-        </div>
